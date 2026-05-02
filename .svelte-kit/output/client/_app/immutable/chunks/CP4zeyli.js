7073b51de254f0198ec73d50d1991e4ddb1b3bca
Gustavo <siqgustavo2@hotmail.com>
2026-05-02 17:56:59 -0300

    Add .gitignore and remove build artifacts from tracking

--- .svelte-kit/output/client/_app/immutable/chunks/CP4zeyli.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1ibaqf0?.base??"/portfolio",a=globalThis.__sveltekit_1ibaqf0?.assets??s??"";export{a,s as b};
--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BpQD0vDw.js","_app/immutable/chunks/DPTbCkPj.js","_app/immutable/chunks/Cd5p3PAn.js","_app/immutable/chunks/CfCg2e5A.js","_app/immutable/chunks/BTLCzigd.js","_app/immutable/chunks/C_QLDBfU.js","_app/immutable/chunks/CP4zeyli.js","_app/immutable/chunks/Dce5S-A0.js"];
-export const stylesheets = [];
-export const fonts = [];
